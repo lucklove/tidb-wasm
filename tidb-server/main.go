394a816c2c14044d37b11a7e57ff2893ac7e2ca5
5kbpers <tangminghua@pingcap.com>
2019-10-26 17:41:17 +0800

    support create session
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- tidb-server/main.go
+++ tidb-server/main.go
@@ -23,10 +23,10 @@ func setup() *Kit {
 		panic("create mock tikv store failed")
 	}
 	session.SetSchemaLease(0)
+	session.SetStatsLease(0)
 	if _, err := session.BootstrapSession(store); err != nil {
 		panic("bootstrap session failed")
 	}
-
 	return NewKit(store)
 }
 
@@ -34,16 +34,28 @@ func main() {
 	k := setup()
 	term := NewTerm()
 
-	js.Global().Set("execute", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	js.Global().Set("createSession", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		fmt.Println("create session")
+		return k.CreateSession()
+	}))
+
+	js.Global().Set("closeSession", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		id := args[0].Int()
+		fmt.Println("close session")
+		k.CloseSession(id)
+		return nil
+	}))
+
+	js.Global().Set("executeSQL", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		start := time.Now()
-		sql := args[0].String()
+		id := args[0].Int()
+		sql := args[1].String()
 		fmt.Println(sql)
-		if rs, err := k.Exec(sql); err != nil {
+		if rs, err := k.Exec(id, sql); err != nil {
 			return term.Error(err)
 		} else if rs == nil {
 			return term.WriteEmpty(time.Now().Sub(start))
-			return nil
-		} else if rows, err := session.ResultSetToStringSlice(context.Background(), k.se, rs); err != nil {
+		} else if rows, err := k.ResultSetToStringSlice(context.Background(), id, rs); err != nil {
 			return term.Error(err)
 		} else {
 			msg := term.WriteRows(rs.Fields(), rows, time.Now().Sub(start))
